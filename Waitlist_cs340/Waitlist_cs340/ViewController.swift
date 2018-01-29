b43f82a29dd5e93bd080c9e416fa7494b5579de8
Naveli Shah <navelishah@nshahmac.nomads.utk.edu>
2018-01-29 12:50:15 -0500

    the real app

---
+++ Waitlist_cs340/Waitlist_cs340/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waitlist_cs340
+//
+//  Created by Naveli Shah on 1/22/18.
+//  Copyright © 2018 Naveli Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
