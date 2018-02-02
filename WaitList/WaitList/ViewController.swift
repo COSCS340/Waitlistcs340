4c4de740d95312a88a2e9a0aea624183892d392c
Avie Desai <aviedesai@aviemac.nomads.utk.edu>
2018-02-02 12:34:25 -0500

    Create Project

---
+++ WaitList/WaitList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaitList
+//
+//  Created by Avie Desai on 2/2/18.
+//  Copyright © 2018 Avie Desai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
